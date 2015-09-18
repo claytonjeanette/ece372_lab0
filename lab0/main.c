ff8e1f09d0df842139d5800764c842b25992a69c
Clayton <cjeanette@email.arizona.edu>
2015-09-18 15:53:14 -0700

    Working Code before clean up

--- lab0/main.c
+++ lab0/main.c
@@ -17,109 +17,118 @@
 #define INPUT 1
 
 //TODO: Define states of the state machine
-typedef enum stateTypeEnum{
+
+//LED_1 = LATDbits.LATD0;
+
+typedef enum stateTypeEnum {
     led1, led2, led3, wait, wait2, debouncePress, debounceRelease, debounceRelease2
 } stateType;
 
 //TODO: Use volatile variables that change within interrupts
 volatile stateType state = led2;
 
-
-
 int main() {
-    
+
     //This function is necessary to use interrupts. 
     enableInterrupts();
-    
+
     //TODO: Write each initialization function
-    initSwitch1(); 
+    initSwitch1();
     initLEDs();
     initTimer2();
     initTimer1();
-    
-    
-    while(1){
-       
+
+
+    while (1) {
+
         //TODO: Implement a state machine to create the desired functionality
-        switch(state){
+        switch (state) {
             case led1:
                 turnOnLED(1); // turns on led1
-                if (IFS1bits.CNDIF == 1){ // IFS1bits.CNDIF == 1)if the switch is pressed
-                          state = debouncePress;              
+                if (PORTDbits.RD6 == 0) { // IFS1bits.CNDIF == 1)if the switch is pressed
+                    state = debouncePress;
+                    IFS0bits.T1IF = 0;
+                    TMR1 = 0;
+                    T1CONbits.TON = 1;
                 }
                 break;
-                
+
             case led2:
                 turnOnLED(2); // turns on led2
-                if (IFS1bits.CNDIF == 1){ // if the switch is pressed
-                  state = debouncePress;
+                if (PORTDbits.RD6 == 0) { // if the switch is pressed
+                    state = debouncePress;
+                    IFS0bits.T1IF = 0;
+                    TMR1 = 0;
+                    T1CONbits.TON = 1;
                 }
                 break;
-                
+
             case led3:
                 turnOnLED(3); // turns on led3
-                if (IFS1bits.CNDIF == 1){ // if the switch is pressed
-                  state = debouncePress;
+                if (PORTDbits.RD6 == 0) { // && PORTDbits.RD6 == 1if the switch is pressed
+                    state = debouncePress;
+                    IFS0bits.T1IF = 0;
+                    TMR1 = 0;
+                    T1CONbits.TON = 1;
                 }
                 break;
-                
+
             case wait:
-                if (PORTDbits.RD6 == 1){
-                    if(IFS0bits.T2IF == 1){ // after two seconds occurs goes to wait 2
-                        state = wait2; 
+                if (PORTDbits.RD6 == 1) {
+                    if (IFS0bits.T1IF == 1) { // after two seconds occurs goes to wait 2
+                        state = wait2;
                     }
-                    if (IFS1bits.CNDIF == 1){ // if button is released before 2 sec go to correct state
-                        state = debounceRelease; 
-                    }  
-                } 
-                break;
-                
-            case wait2:
-                state=led3;
-                if (IFS1bits.CNDIF == 0) {  // wait until button is released
-                   // state = debounceRelease2;
+                    // if button is released before 2 sec go to correct state
+                    else  state = debounceRelease;
+                    
                 }
+                break;
+
+            case wait2:
+
+                 // wait until button is released
+                    state = debounceRelease2;
+              
                 // Then go to debounceRelease2 that will go to correct state
                 break;
-                
+
             case debouncePress:
-                if (T2CONbits.ON == 1){// waits for delays a few ms
-                    state = wait;// go to the first wait state
-                }
-                break;
+                delayMs(10);
+                // waits for delays a few ms
+                    state = wait; // go to the first wait state
                 
+                break;
+
             case debounceRelease:
-               if (T2CONbits.ON == 1){ // waits for delay
-                 if ( LATDbits.LATD0 == 1 ){//if on led1
-                     state = led2;
-                  }
-                 else if ( LATDbits.LATD1 == 1 ){// if on led2
-                     state = led3;
-                 }
-                 else if (LATDbits.LATD2 == 1 ){// if on led3
-                      state = led1;
-                 }
-               }
+                delayMs(10);
+                T1CONbits.TON = 0;// waits for delay
+                    if (LATDbits.LATD0 == 1) {//if on led1
+                        state = led2;
+                    } else if (LATDbits.LATD1 == 1) {// if on led2
+                        state = led3;
+                    } else if (LATDbits.LATD2 == 1) {// if on led3
+                        state = led1;
+                    }
+                
                 // the release case for under 2 seconds to go forward
                 break;
-                
+
             case debounceRelease2:
-                
-                if (T2CONbits.ON == 0 && LATDbits.LATD0 == 1 ){// delays a few ms
-                    state = led1;
-                }
-                else if (T2CONbits.ON == 0 && LATDbits.LATD1 == 1 ){// delays a few ms
-                    state = led2;
-                }
-                else if (T2CONbits.ON == 0 && LATDbits.LATD2 == 1 ){// delays a few ms
+                delayMs(10);
+                T1CONbits.TON = 0;
+                if (LATDbits.LATD0 == 1) {// delays a few ms
                     state = led3;
+                } else if (LATDbits.LATD1 == 1) {// delays a few ms
+                    state = led1;
+                } else if (LATDbits.LATD2 == 1) {// delays a few ms
+                    state = led2;
                 }
                 // the release case for over 2 seconds to go backwards
                 break;
-                
+
         }
     }
-    
+
     return 0;
 }
 
--- lab0/timer.c
+++ lab0/timer.c
@@ -11,24 +11,24 @@ void initTimer1(){
     //TODO: Initialize Timer 1 to have a period of
     // 1 second. Enable its interrupt
     TMR1 = 0;// clears Timer1
-    PR1 = 1221*2;// Initialize PR1 for a 1 sec timer
+    PR1 = 1221*4;// Initialize PR1 for a 1 sec timer
     T1CONbits.TCKPS = 3; // Initialize pre-scalar
     T1CONbits.TCS = 0; // Setting the oscillator for Timer 1
-    IEC0bits.T1IE = 1;// Enable the interrupt
-    IFS0bits.T1IF = 0;// Put the flag down
-    IPC1bits.T1IP = 3;// Configure the Interrupt Priority
+    //IEC0bits.T1IE = 1;// Enable the interrupt
+   // IFS0bits.T1IF = 0;// Put the flag down
+   // IPC1bits.T1IP = 3;// Configure the Interrupt Priority
     T1CONbits.TON = 1;// Turn the timer on
 }
 
 initTimer2(){
     //TODO: Initialize Timer 2.
     TMR2 = 0;// clears Timer2
-    PR2 = 1221*2;// Initialize Priority Register 1 for a 2 sec timer
-    T2CONbits.TCKPS = 3; // Initialize pre-scalar
+   // Initialize Priority Register 1 for a 2 sec timer
+    T2CONbits.TCKPS = 0; // Initialize pre-scalar
     T2CONbits.TCS = 0; // Setting the oscillator for Timer 2
-    IEC0bits.T2IE = 1;// Enable the interrupt for Timer 2
-    IFS0bits.T2IF = 0;// Put the flag down
-    IPC2bits.T2IP = 3;// Configure the Interrupt Priority
+   // IEC0bits.T2IE = 1;// Enable the interrupt for Timer 2
+   // IFS0bits.T2IF = 0;// Put the flag down
+   // IPC2bits.T2IP = 3;// Configure the Interrupt Priority
     T2CONbits.TON = 1;// Turn the timer on
     
 }
